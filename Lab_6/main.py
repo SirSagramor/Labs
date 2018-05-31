c4c83a7c1a2615ca5ee4ff9c8a1da10c0deafb1f
SirSagramor <sir.sagramor@gmail.com>
2018-05-31 18:18:18 +0300

    problem with recursion v2

--- Lab_6/main.py
+++ Lab_6/main.py
@@ -1,56 +1,61 @@
 import math
+
+
 # x, y - координаты искомой точки, х1, у1 - координаты центра круга, R - радиус
 def dotInCircle(x, y, x1, y1, R):
-    if ((x - x1)**2 + (y - y1)**2) <= R**2:
+    if ((x - x1) ** 2 + (y - y1) ** 2) <= R ** 2:
         return True
     return False
+
+
+# нахождение координат по долготе и широте
 def findXY(latitude, longitude):
-    return 6371*math.cos(latitude)*math.cos(longitude), 6371*math.cos(latitude)*math.sin(longitude)
+    return 6371 * math.cos(latitude) * math.cos(longitude), 6371 * math.cos(latitude) * math.sin(longitude)
+
+
+# self.north = (x, y + h)  # верх
+# self.south = (x, y - h)  # низ
+# self.west = (x - w, y)  # лево
+# self.east = (x + w, y)  # право
+
 
 class Leaf:
-    def __init__(self, cord, type = None, subtype = None, name = None, adress = None):
-        self.cord = cord
+    def __init__(self, cord, type=None, subtype=None, name=None, adress=None):
         self.x = cord[0]
         self.y = cord[1]
         self.type = type
         self.subtype = subtype
         self.name = name
-        self. adress = adress
+        self.adress = adress
     def __str__(self):
-        return 'Node: ['+str(self.x)+' '+str(self.y)+' type: '+self.type+' subtype: '+self.subtype+' name: '+self.name+' adress: '+self.adress+']'
+        return 'Node: [' + str(self.x) + ' ' + str(
+            self.y) + ' type: ' + self.type + ' subtype: ' + self.subtype + ' name: ' + self.name + ' adress: ' + self.adress + ']'
 
 
 class Node:
-    def __init__(self, cord, h, w, deapth, dots):
-        self.cord = cord
-        self.north = (cord[0], cord[1] + h) #верх
-        self.south = (cord[0], cord[1] - h) #низ
-        self.west = (cord[0] - w, cord[1]) #лево
-        self.east = (cord[0] + w, cord[1]) #право
-        self.deapth = deapth
-        self.dots = dots
-        self.leafes = []
+    def __init__(self, cord, h, w, deapth):
+        self.x = cord[0]
+        self.y = cord[1]
         self.h = h
         self.w = w
+        self.deapth = deapth
+        self.dots = 0
+        self.leafes = []
     def addLeaf(self, leaf):
         self.leafes.append(leaf)
         self.dots += 1
 
+
 class RTree:
-    def __init__(self, cord, h, w, deapth, dots):
-        self.cord = cord
+    def __init__(self, cord, h, w, deapth):
         self.x = cord[0]
         self.y = cord[1]
-        self.north = (cord[0], cord[1] + h)  # верх
-        self.south = (cord[0], cord[1] - h)  # низ
-        self.west =  (cord[0] - w, cord[1])  # лево
-        self.east =  (cord[0] + w, cord[1])  # право
         self.h = h
         self.w = w
         self.deapth = deapth
-        self.dots = dots
+        self.dots = 0
         self.children = [None, None]
-        self.root = Node(cord, h, w, deapth, dots)
+        self.root = Node(cord, h, w, deapth)
 
     def add(self, leaf):
         # если нет корня, то дерево поделено уже и нужно точку добавить в одну из следующик коробок
@@ -68,8 +73,8 @@ class RTree:
         # если количество точек больше пяти, то делим коробку на 2 части и добавляем туда точки
         elif self.dots > 5:
             if self.deapth % 2:
-                self.children[0] = RTree(((self.x - self.w) / 2, self.y), self.w / 2, self.h, self.deapth + 1, 0)
-                self.children[1] = RTree(((self.x + self.w) / 2, self.y), self.w / 2, self.h, self.deapth + 1, 0)
+                self.children[0] = RTree(((self.x - self.w) / 2, self.y), self.w / 2, self.h, self.deapth + 1)
+                self.children[1] = RTree(((self.x + self.w) / 2, self.y), self.w / 2, self.h, self.deapth + 1)
                 for i in self.root.leafes:
                     if i.x > self.x:
                         self.children[0].add(i)
@@ -80,8 +85,8 @@ class RTree:
                 else:
                     self.children[1].add(leaf)
             else:
-                self.children[0] = RTree((self.x, (self.y + self.h) / 2), self.w, self.h / 2, self.root.deapth + 1, 0)
-                self.children[1] = RTree((self.x, (self.y - self.h) / 2), self.w, self.h / 2, self.root.deapth + 1, 0)
+                self.children[0] = RTree((self.x, (self.y + self.h) / 2), self.w, self.h / 2, self.root.deapth + 1)
+                self.children[1] = RTree((self.x, (self.y - self.h) / 2), self.w, self.h / 2, self.root.deapth + 1)
                 for i in self.root.leafes:
                     if i.y > self.y:
                         self.children[0].add(i)
@@ -99,16 +104,12 @@ class RTree:
         return
 
 
-
-tree = RTree((0, 0), 6372, 6372, 1, 0)
+tree = RTree((0, 0), 6372, 6372, 1)
 
 f = open('ukraine_poi.csv')
 for line in f:
     info = line.split(';')
     leaf = Leaf(findXY(float(line[0]), float(line[1])), line[2], line[3], line[4], line[5])
     tree.add(leaf)
-
-
-
 
 f.close()
