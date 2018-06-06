fc7c0c13c9825ee91bb0d3a569ff2f877a2d3d0c
Denys Pravdyvyj <30390172+SO1T@users.noreply.github.com>
2018-06-06 20:26:15 +0300

    Add files via upload

--- Lab_6/main.py
+++ Lab_6/main.py
@@ -1,103 +1,193 @@
 import math
+import time
+import queue
+import sys
+import argparse
 
-# x, y - координаты искомой точки, х1, у1 - координаты центра круга, R - радиус
-def dotInCircle(x, y, x1, y1, R):
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--db', default='ukraine_poi.csv')
+    parser.add_argument('--lat', type=float, default=50.45130913)
+    parser.add_argument('--long', type=float, default=30.45735345)
+    parser.add_argument('--size', type=float, default=3)
+    parser.add_argument('--type')
+    return parser
+
+# cord - center circle, x1, y1 - dot, R - radius
+def dotInCircle(cord, x1, y1, R):
+    x, y = cord[0], cord[1]
     if ((x - x1) ** 2 + (y - y1) ** 2) <= R ** 2:
         return True
     return False
 
-# нахождение координат по долготе и широте
+# check is circle in rect?
+def circleInRect(cordCircle, cordRect, h, w, R):
+    x, y = cordCircle[0], cordCircle[1]
+    x1, y1 = cordRect[0], cordRect[1]
+
+    circleDistanceX = abs(x - x1)
+    circleDistanceY = abs(y - y1)
+
+    if circleDistanceX > w + R or circleDistanceY > h + R:
+        return False
+
+    if circleDistanceX <= w or circleDistanceY <= h:
+        return True
+
+    cornerDistance_sq = (circleDistanceX - w) ** 2 + (circleDistanceY - h) ** 2
+
+    return cornerDistance_sq <= R ** 2
+
+# coordinats in decart system
 def findXY(latitude, longitude):
     return 6371 * math.cos(latitude) * math.cos(longitude), 6371 * math.cos(latitude) * math.sin(longitude)
 
-# self.north = (x, y + h)  # верх
-# self.south = (x, y - h)  # низ
-# self.west = (x - w, y)  # лево
-# self.east = (x + w, y)  # право
+# iterator
+def make_counter():
+    i = 0
 
+    def counter():  # counter() is a closure
+        nonlocal i
+        i += 1
+        return i
+
+    return counter
+
+# Leaf with latitude, longtude and information
 class Leaf:
-    def __init__(self, cord, type=None, subtype=None, name=None, adress=None):
-        self.x = cord[0]
-        self.y = cord[1]
+    def __init__(self, cord, type, subtype, name, adress):
+        self.cord = cord
+        xy = findXY(cord[0], cord[1])
+        self.x = xy[0]
+        self.y = xy[1]
         self.type = type
         self.subtype = subtype
         self.name = name
         self.adress = adress
-    def __str__(self):
-        return 'Node: [' + str(self.x) + ' ' + str(
-            self.y) + ' type: ' + self.type + ' subtype: ' + self.subtype + ' name: ' + self.name + ' adress: ' + self.adress + ']'
 
-class RTree:
-    def __init__(self, cord, h, w, deapth):
+    def __str__(self):
+        return 'Leaf: [latitude: %f, longtude: %f, x: %f, y: %f type: %s, subtype: %s, name: %s, adress: %s]' % \
+               (self.cord[0], self.cord[1], self.x, self.y, self.type, self.subtype, self.name, self.adress)
+
+# Rect Node
+class Node:
+    def __init__(self, cord, h, w, depth, n):
         self.x = cord[0]
         self.y = cord[1]
         self.h = h
         self.w = w
-        self.deapth = deapth
+        self.depth = depth
+        self.n = n
         self.dots = 0
         self.children = [None, None]
         self.divided = False
         self.leafes = []
 
+# RTree
+class RTree():
+    def __init__(self, cord=(0, 0), h=10000, w=10000, n=100):
+        self.root = Node(cord, h, w, 1, n)
     def add(self, leaf):
-        # если дерево поделено уже то нужно точку добавить в одну из следующик коробок
-        if self.divided:
-            if self.deapth % 2:
-                if self.x - self.w < leaf.x and self.x > leaf.x:
-                    self.children[0].add(leaf)
-                else:
-                    self.children[1].add(leaf)
-            else:
-                if self.y + self.h > leaf.y and self.y < leaf.y:
-                    self.children[0].add(leaf)
-                else:
-                    self.children[1].add(leaf)
-        # если количество точек больше пяти, то делим коробку на 2 части и добавляем туда точки
-        elif self.dots > 5:
-            if self.deapth % 2:
-                self.children[0] = RTree(((self.x - self.w) / 2, self.y), self.w / 2, self.h, self.deapth + 1)
-                self.children[1] = RTree(((self.x + self.w) / 2, self.y), self.w / 2, self.h, self.deapth + 1)
-                for i in self.leafes:
-                    if i.x > self.x:
-                        self.children[0].leafes.append(i)
-                        self.children[0].dots += 1
+        temp = self.root
+        # here check for the fact that the node is not the initial root
+        if temp.children[0]:
+            while temp.divided:
+                if temp.depth % 2:
+                    if temp.children[0].x + temp.children[0].w > leaf.x:
+                        temp = temp.children[0]
                     else:
-                        self.children[1].leafes.append(i)
-                        self.children[1].dots += 1
-                if self.x - self.w < leaf.x and self.x > leaf.x:
-                    self.children[0].leafes.append(leaf)
-                    self.children[0].dots += 1
+                        temp = temp.children[1]
                 else:
-                    self.children[1].leafes.append(leaf)
-                    self.children[1].dots += 1
-            else:
-                self.children[0] = RTree((self.x, (self.y + self.h) / 2), self.w, self.h / 2, self.deapth + 1)
-                self.children[1] = RTree((self.x, (self.y - self.h) / 2), self.w, self.h / 2, self.deapth + 1)
-                for i in self.leafes:
-                    if i.y > self.y:
-                        self.children[0].leafes.append(i)
-                        self.children[0].dots += 1
+                    if temp.children[0].y - temp.children[0].h < leaf.y:
+                        temp = temp.children[0]
                     else:
-                        self.children[1].leafes.append(i)
-                        self.children[1].dots += 1
-                if self.y + self.h > leaf.y and self.y < leaf.y:
-                    self.children[0].leafes.append(leaf)
-                    self.children[0].dots += 1
+                        temp = temp.children[1]
+        # if there are more than n points in the node in which you want to add, divide it into 2 parts, adding the leaves to these nodes
+        if temp.dots == temp.n:
+            if temp.depth % 2:
+                temp.leafes.sort(key=lambda x: x.x)
+                mid = (temp.leafes[temp.n // 2 - 1].x + temp.leafes[temp.n // 2].x) / 2
+                x0 = (mid + temp.x - temp.w) / 2
+                x1 = (mid + temp.x + temp.w) / 2
+                temp.children[0] = Node((x0, temp.y), temp.h, x0 - (temp.x - temp.w), temp.depth + 1, temp.n)
+                temp.children[1] = Node((x1, temp.y), temp.h, (temp.x + temp.w) - x1, temp.depth + 1, temp.n)
+                temp.children[0].leafes = temp.leafes[:temp.n // 2]
+                temp.children[1].leafes = temp.leafes[temp.n // 2:]
+                temp.children[0].dots += temp.n // 2
+                temp.children[1].dots += temp.n // 2
+                if mid > leaf.x:
+                    temp.children[0].leafes.append(leaf)
+                    temp.children[0].dots += 1
                 else:
-                    self.children[1].leafes.append(leaf)
-                    self.children[1].dots += 1
-            self.divided = True
-            self.leafes = []
-        # иначе добавить листок в текущюю ноду
+                    temp.children[1].leafes.append(leaf)
+                    temp.children[1].dots += 1
+            else:
+                temp.leafes.sort(key=lambda y: y.y)
+                mid = (temp.leafes[temp.n // 2 - 1].y + temp.leafes[temp.n // 2].y) / 2
+                y0 = (mid + temp.y + temp.h) / 2
+                y1 = (mid + temp.y - temp.h) / 2
+                temp.children[0] = Node((temp.x, y0), (temp.y + temp.h) - y0, temp.w, temp.depth + 1, temp.n)
+                temp.children[1] = Node((temp.x, y1), y1 - (temp.y - temp.h), temp.w, temp.depth + 1, temp.n)
+                temp.children[0].leafes = temp.leafes[temp.n // 2:]
+                temp.children[1].leafes = temp.leafes[:temp.n // 2]
+                temp.children[0].dots += temp.n // 2
+                temp.children[1].dots += temp.n // 2
+                if mid < leaf.y:
+                    temp.children[0].leafes.append(leaf)
+                    temp.children[0].dots += 1
+                else:
+                    temp.children[1].leafes.append(leaf)
+                    temp.children[1].dots += 1
+            temp.divided = True
+            temp.leafes = []
         else:
-            self.leafes.append(leaf)
-            self.dots += 1
+            temp.leafes.append(leaf)
+            temp.dots += 1
+    def findCord(self ,cord, R, type=None):
+        out = []
+        q = queue.Queue()
+        q.put(self.root)
+        while not q.empty():
+            temp = q.get()
+            if temp.divided:
+                if circleInRect(cord, (temp.children[0].x, temp.children[0].y), temp.children[0].h, temp.children[0].w, R):
+                    q.put(temp.children[0])
+                if circleInRect(cord, (temp.children[1].x, temp.children[1].y), temp.children[1].h, temp.children[1].w, R):
+                    q.put(temp.children[1])
+            else:
+                if type:
+                    out += [el for el in temp.leafes if dotInCircle(cord, el.x, el.y, R) and el.type == type]
+                else:
+                    out += [el for el in temp.leafes if dotInCircle(cord, el.x, el.y, R)]
 
-tree = RTree((0, 0), 6372, 6372, 1)
-f = open('ukraine_poi.csv')
-for line in f:
-    info = line.split(';')
-    leaf = Leaf(findXY(float(line[0]), float(line[1])), line[2], line[3], line[4], line[5])
-    tree.add(leaf)
+        return out
 
-f.close()
+if __name__ == '__main__':
+
+    start_time = time.time()
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    tree = RTree()
+    f = open(namespace.db)
+    i = make_counter()
+    for line in f:
+        info = line.split(';')
+        info[0] = info[0].replace(',', '.')
+        info[1] = info[1].replace(',', '.')
+        i()
+        try:
+            leaf = Leaf((float(info[0]), float(info[1])), info[2], info[3], info[4], info[5])
+            tree.add(leaf)
+        except ValueError:
+            print("Value error on line:", i())
+
+    out = tree.findCord(findXY(namespace.lat, namespace.long), namespace.size * 5, namespace.type)
+    
+    print("\nCoordinats in decart system:",findXY(namespace.lat, namespace.long))
+    print("We found %s next entities in the sector:" % (len(out)))
+    for el in out:
+        print(el)
+    print("\n\nTime: %s seconds" % (round(time.time() - start_time, 2)))
+
+    f.close()
